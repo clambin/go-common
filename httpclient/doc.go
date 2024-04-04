eeeb9574b320e967172f7fa088961fd8748f64ee
Christophe Lambin <christophe.lambin@gmail.com>
2024-04-05 00:32:32 +0200

    build: update dependabot.yml based on current modules

--- httpclient/doc.go
+++
@@ -1,6 +0,0 @@
-/*
-Package httpclient provides a set of http.RoundTripper implementations that extend http.Client's transport layer.
-
-RoundTrippers can be chained together to create a complex client middleware that is transparent at the application layer.
-*/
-package httpclient
--- httpserver/middleware/doc.go
+++
@@ -1,3 +0,0 @@
-// Package middleware contains HTTP middleware. Some of these are used by httpserver, but can
-// be used by any http router that supports net/http-compatible middleware.
-package middleware
