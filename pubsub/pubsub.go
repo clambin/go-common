cf8ef730b8a4ff52939a9fc7721b2089d573d6bc
Christophe Lambin <christophe.lambin@gmail.com>
2024-12-15 14:06:24 +0100

    feat(pubsub): subscription channels are read-only.

--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -3,22 +3,22 @@ package pubsub
 import "sync"
 
 type Publisher[T any] struct {
-	clients map[chan T]struct{}
+	clients map[<-chan T]chan T
 	lock    sync.RWMutex
 }
 
-func (p *Publisher[T]) Subscribe() chan T {
+func (p *Publisher[T]) Subscribe() <-chan T {
 	p.lock.Lock()
 	defer p.lock.Unlock()
 	if p.clients == nil {
-		p.clients = make(map[chan T]struct{})
+		p.clients = make(map[<-chan T]chan T)
 	}
 	ch := make(chan T)
-	p.clients[ch] = struct{}{}
+	p.clients[ch] = ch
 	return ch
 }
 
-func (p *Publisher[T]) Unsubscribe(ch chan T) {
+func (p *Publisher[T]) Unsubscribe(ch <-chan T) {
 	p.lock.Lock()
 	defer p.lock.Unlock()
 	delete(p.clients, ch)
@@ -27,7 +27,7 @@ func (p *Publisher[T]) Unsubscribe(ch chan T) {
 func (p *Publisher[T]) Publish(data T) {
 	p.lock.RLock()
 	defer p.lock.RUnlock()
-	for ch := range p.clients {
+	for _, ch := range p.clients {
 		ch <- data
 	}
 }
